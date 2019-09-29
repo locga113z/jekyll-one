b3a7b90997ab19e9098644f8318e4f80cfbd241b
locga113z <locga113z@gmail.com>
2019-09-29 18:05:56 +0700

    Mr Hyde was here …

--- js/back-to-top.js
+++
@@ -1,2 +0,0 @@
-$(function() { $(window).scroll(function() { if($(this).scrollTop()>100) { $('#ScrollToTop').fadeIn()} else { $('#ScrollToTop').fadeOut();}});
-$('#ScrollToTop').click(function(){$('html,body').animate({scrollTop:0},1000);return false})});
